3526f03790694d63eed6132a6f42edbdb245eb1f
nibalabaza <nibal.abazli@gmail.com>
2022-12-15 09:46:06 +0100

    tooltip

--- event-paradise/index.js
+++ event-paradise/index.js
@@ -1,67 +1,48 @@
+    const tooltipOptions = {
+        title: "This field is required",
+        placement: "top"
+    };
+    let tooltipsMessages = [];
     let toolEl = document.querySelectorAll(".myTooltip");
-    toolEl.forEach(element => {
-        new bootstrap.Tooltip(element)
+    toolEl.forEach(tooltipEl => {
+        let tooltipEls = new bootstrap.Tooltip(tooltipEl, tooltipOptions)
+        tooltipsMessages.push(tooltipEls)
     })
-
 
     let form = document.querySelector("form");
     console.log("form", form)
     let elements = form.elements;
 
     for (const element of elements) {
+        console.log(element.id)
         let type = element.type;
         let date = new Date;
         if (type != "submit") {
             element.addEventListener("invalid", (event) => {
                 event.preventDefault();
-
+                //tooltipValidation();
+                
                 if (element.validity.valueMissing) {
-                    showTooltip()
-                    element.style.borderColor = "red";
-                    console.log("C'est champ est obligatoir!")
-                } else if (type == "date" || new Date(date.toDateString()) > new Date(date.toDateString())) {
-                    console.log("Doit étre égale ou supéreure")
-                } else if (type == "number" || element.rangeUnderflow) {
-                    console.log("Doit être positif")
-                } else {
-                    console.log(element.style.borderColor = "green");
+                    element.classList.add("is-valid");         
                 }
+                
 
             });
         }
     }
+
+    
+
+    function toast() {
+        const toastTrigger = document.getElementById('toastTrigger')
+        const toast = new bootstrap.Toast(toastTrigger);
+        toast.show()
+    }
+
     form.addEventListener("submit", (event) => {
         event.preventDefault();
-        //toast();
-        console.log("implement form reset");
+        toast();
         console.log("implement toaster");
     });
 
-    // function toast() {
-    //     const toastEl = document.getElementById('toast');
-    //     const toast = new bootstrap.Toast(toastEl);
-    //     toast.show();
-    // }
-
-
-    function showTooltip() {
-        hideTooltip();
-        let form = document.querySelector("form");
-        let elements = form.elements;
-        for (const element of elements) {
-            element.classList.add("tooltip-visible");
-        }
-
-    }
-
-    function hideTooltip() {
-        let el = document.querySelector(".tooltip-visible");
-        if (el) {
-            el.classList.remove("tooltip-visible");
-        }
-    }
-
-    const els = document.querySelectorAll(".myTooltip");
-    for (let i = 0; i < els.length; i++) {
-        els[i].addEventListener('hoover', showTooltip);
-    }
+  
